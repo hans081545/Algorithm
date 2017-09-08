003cdf90aecca020a964018527452e28e65df420
Kyungmin Han <hans@mtome.co.kr>
2017-09-08 11:42:40 +0900

    ADD Selection Sort
    Signed-off-by: Kyungmin Han <hans@mtome.co.kr>

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## List
 
 * Bubble Sort
+* Selection Sort
---
+++ SelectionSort.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  SelectionSort
+//
+//  Created by Kyungmin Han on 2017. 9. 8..
+//  Copyright © 2017년 han. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+void printInsertNum(int insertArr[], int max);
+void sort(int insertArr[], int max);
+
+int main(int argc, const char * argv[]) {
+    
+    int arr[10];
+    int max = 0;
+    
+    cout << "입력할 숫자의 개수를 입력해 주세요. : ";
+    cin >> max;
+    
+    for (int i=0; i<max; i++) {
+        cout << i+1 << " : ";
+        cin >> arr[i];
+    }
+    
+    printInsertNum(arr, max);
+    sort(arr, max);
+    return 0;
+}
+
+void printInsertNum (int insertArr[], int max) {
+    for (int i=0; i<max; i++) {
+        cout << insertArr[i] << " ";
+    }
+    cout << endl;
+}
+
+void sort(int insertArr[], int max) {
+    
+    int temp;
+    
+    cout << "insert number : ";
+    for (int i=0; i<max; i++) {
+        cout << insertArr[i] << " ";
+    }
+    
+    for (int i=0; i<max-1; i++) {
+        for (int j=i+1; j<max; j++) {
+            if (insertArr[i] > insertArr[j]) {
+                temp = insertArr[i];
+                insertArr[i] = insertArr[j];
+                insertArr[j] = temp;
+            }
+        }
+        
+        //start sort log//
+        cout << "\n round " << i << " : ";
+        for (int j=0; j<max; j++) {
+            cout << insertArr[j] << " ";
+        }
+        //end sort log//
+    }
+    
+    cout << endl;
+}
