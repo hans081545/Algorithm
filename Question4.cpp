5a932a36990076abd2bf01f9772ce135c1ca3c93
Kyungmin Han <hans@mtome.co.kr>
2017-09-20 11:53:06 +0900

    Edit 마지막 인자만 출력하게끔, fibonacci 알고리즘 방식
    Signed-off-by: Kyungmin Han <hans@mtome.co.kr>

--- Question4.cpp
+++ Question4.cpp
@@ -5,7 +5,7 @@
 //  Created by Kyungmin Han on 2017. 9. 19..
 //  Copyright © 2017년 han. All rights reserved.
 //
-// Question3
+// Question4
 // 피보나치 수 구하기.
 // ex) f(1) = 1, f(2) = 1, f(n+2) = f(n) + f(n+1)...
 // 피보나치 수열의 n번째 항 f(n)을 계산하여 반환하는 함수를 작성하고, f(20), f(50)을 구하라.
@@ -14,35 +14,25 @@
 
 using namespace std;
 
-void fibonacci(long param1, long param2, int currentIndex, int max);
+long fibonacci(int n);
 
 int main(int argc, const char * argv[]) {
     
-    int max=0;
-    int currentIndex=3;
-    long f1=1, f2=1;
+    int n=0;
     
     cout << "input : ";
-    cin >> max;
+    cin >> n;
     
-    cout << "f(1) : " << f1 << ", f(2) : " << f2;
-    
-    if (max > 2) {
-        fibonacci(f1, f1+f2, currentIndex, max);
-    }
-    cout << endl;
+    cout << "f(" << n << ") : " << fibonacci(n) << endl;
     
     return 0;
 }
 
-void fibonacci(long param1, long param2, int currentIndex, int max) {
+long fibonacci(int n) {
     
-    cout << ", f(" << currentIndex << ") : ";
-    cout << param2;
-    
-    int increageIndex = currentIndex+1;
-    
-    if (increageIndex <= max) {
-        fibonacci(param2, param1+param2, increageIndex, max);
+    if (n==1 || n==2) {
+        return 1;
     }
+    
+    return (fibonacci(n-1) + fibonacci(n-2));
 }
